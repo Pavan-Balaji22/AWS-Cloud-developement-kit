aa22648fc2c84ff2cf76568a9ddd3d6dc8324513
Pavan Balaji Kumar <pavanbalaji22@gmail.com>
2024-03-16 19:26:34 -0400

    baseline

--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,24 @@
 // import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lf from 'aws-cdk-lib/aws-lakeformation';s
 
 export interface LakeformationProps {
   // Define construct properties here
+  DataSource:String,
+  principalarn:String,
+  permissions:String,
+  database:String,
+  table:String
+
 }
 
 export class Lakeformation extends Construct {
 
-  constructor(scope: Construct, id: string, props: LakeformationProps = {}) {
+  constructor(scope: Construct, id: string, props: LakeformationProps) {
     super(scope, id);
 
+    const resource = new lf.CfnResource(this,'database',{})
     // Define construct contents here
 
     // example resource
