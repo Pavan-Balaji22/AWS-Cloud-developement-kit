1306509315eac1e820ff328336ca5cbb69d3453c
Pavan Balaji Kumar <pavanbalaji22@gmail.com>
2024-03-16 19:26:34 -0400

    Updated lakeformation construct

--- lib/index.ts
+++ lib/index.ts
@@ -1,29 +1,39 @@
-// import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-import * as lf from 'aws-cdk-lib/aws-lakeformation';s
+import * as lf from 'aws-cdk-lib/aws-lakeformation';
 
 export interface LakeformationProps {
   // Define construct properties here
-  DataSource:String,
-  principalarn:String,
-  permissions:String,
-  database:String,
-  table:String
-
+  DataLocation:lf.CfnPrincipalPermissions.DataLocationResourceProperty,
+  principalarn:lf.CfnPrincipalPermissions.DataLakePrincipalProperty,
+  permissions:string [],
+  permissionGrant:string [],
+  resource: lf.CfnPrincipalPermissions.ResourceProperty,
+  catalog: string,
+  S3bucket: string,
+  RegisterRoleArn: string
 }
 
 export class Lakeformation extends Construct {
 
   constructor(scope: Construct, id: string, props: LakeformationProps) {
     super(scope, id);
+    
+    // Registering bucket with given role
+    const lfresource : lf.CfnResource = new lf.CfnResource(this,'LfResource',{resourceArn:props.S3bucket,
+    useServiceLinkedRole: false,
+    roleArn:props.RegisterRoleArn});
 
-    const resource = new lf.CfnResource(this,'database',{})
-    // Define construct contents here
+    // Adding permissions to resource with given IAM role
+    const permissions = new lf.CfnPrincipalPermissions(this,'LfPermissions',{
+      resource:props.resource,
+      permissions:props.permissions,
+      permissionsWithGrantOption:props.permissionGrant,
+      principal:props.principalarn,
+      catalog:props.catalog});
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LakeformationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    function addDependancy(temp:string) {
+      permissions.addDependsOn(lfresource);
+    }
+
   }
 }
