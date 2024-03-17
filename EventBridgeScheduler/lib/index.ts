7001d015094683baf0c5aa7df5fae0585aaa5d1a
Pavan Balaji Kumar <pavanbalaji22@gmail.com>
2024-03-17 18:32:33 -0400

    Fixed formatting

--- EventBridgeScheduler/lib/index.ts
+++ EventBridgeScheduler/lib/index.ts
@@ -1,79 +1,82 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as scheduler from 'aws-cdk-lib/aws-scheduler';
-
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as scheduler from "aws-cdk-lib/aws-scheduler";
 
 export interface EventBridgeSchedulerProps {
-
-  readonly schedulerProperties : scheduler.CfnScheduleProps;
+  readonly schedulerProperties: scheduler.CfnScheduleProps;
   readonly name: string;
-  readonly deadLetterQueue:scheduler.CfnSchedule.DeadLetterConfigProperty;
-  readonly executionRole:string;
-  readonly kmsKey?: string; 
+  readonly deadLetterQueue: scheduler.CfnSchedule.DeadLetterConfigProperty;
+  readonly executionRole: string;
+  readonly kmsKey?: string;
   readonly expression: string;
-  readonly flexibleWindow:number;
-  readonly endDate?:string;
-  readonly timeZone?:string;
-  readonly startDate?:string;
-  readonly state?:string;
-  readonly scheduleGroup?:string;
-  readonly targetRetry:scheduler.CfnSchedule.RetryPolicyProperty
+  readonly flexibleWindow: number;
+  readonly endDate?: string;
+  readonly timeZone?: string;
+  readonly startDate?: string;
+  readonly state?: string;
+  readonly scheduleGroup?: string;
+  readonly targetRetry: scheduler.CfnSchedule.RetryPolicyProperty;
   readonly targetApiCall?: string;
-  readonly targetResourceArn?:string;
-  readonly targetInput:string;
-  readonly targetResourceParameters?:scheduler.CfnSchedule.EcsParametersProperty | scheduler.CfnSchedule.KinesisParametersProperty| scheduler.CfnSchedule.EventBridgeParametersProperty | scheduler.CfnSchedule.SqsParametersProperty | scheduler.CfnSchedule.SageMakerPipelineParametersProperty;
-
-
+  readonly targetResourceArn?: string;
+  readonly targetInput: string;
+  readonly targetResourceParameters?:
+    | scheduler.CfnSchedule.EcsParametersProperty
+    | scheduler.CfnSchedule.KinesisParametersProperty
+    | scheduler.CfnSchedule.EventBridgeParametersProperty
+    | scheduler.CfnSchedule.SqsParametersProperty
+    | scheduler.CfnSchedule.SageMakerPipelineParametersProperty;
 }
 
 export class EventBridgeScheduler extends Construct {
+  public readonly attr: scheduler.CfnSchedule;
+  window: scheduler.CfnSchedule.FlexibleTimeWindowProperty = { mode: "OFF" };
+  target: scheduler.CfnSchedule.TargetProperty;
+  schedulerProperties: scheduler.CfnScheduleProps;
 
-  public readonly attr:scheduler.CfnSchedule;
-  window:scheduler.CfnSchedule.FlexibleTimeWindowProperty = {mode:"OFF"};
-  target:scheduler.CfnSchedule.TargetProperty;
-  schedulerProperties:scheduler.CfnScheduleProps
-
-  
   constructor(scope: Construct, id: string, props: EventBridgeSchedulerProps) {
     super(scope, id);
-    
+
     if (props.flexibleWindow) {
       this.window = {
-        mode:"FLEXIBLE",
-        maximumWindowInMinutes:props.flexibleWindow
-      }
+        mode: "FLEXIBLE",
+        maximumWindowInMinutes: props.flexibleWindow,
+      };
     }
 
-    if(props.targetApiCall && props.targetResourceArn){
-      throw new Error("Only one targetApiCall or targetResourceArn variable can be set");
-      
+    if (props.targetApiCall && props.targetResourceArn) {
+      throw new Error(
+        "Only one targetApiCall or targetResourceArn variable can be set"
+      );
     }
 
-    if(props.targetApiCall){
+    if (props.targetApiCall) {
       this.target = {
-        arn:`arn:aws:scheduler:::aws-sdk:targetApiCall`,
-        roleArn:props.executionRole,
-        input:props.targetInput,
-        retryPolicy:props.targetRetry,
-        deadLetterConfig:props.deadLetterQueue,
-      } 
+        arn: `arn:aws:scheduler:::aws-sdk:targetApiCall`,
+        roleArn: props.executionRole,
+        input: props.targetInput,
+        retryPolicy: props.targetRetry,
+        deadLetterConfig: props.deadLetterQueue,
+      };
     }
 
-    const schedulerProperties:scheduler.CfnScheduleProps = {name:props.name,
-    flexibleTimeWindow: this.window,
-    target:this.target,
-    scheduleExpression:props.expression,
-
+    const schedulerProperties: scheduler.CfnScheduleProps = {
+      name: props.name,
+      flexibleTimeWindow: this.window,
+      target: this.target,
+      scheduleExpression: props.expression,
     };
-  
-    this.attr = new scheduler.CfnSchedule(this,'scheduleOne-1', schedulerProperties)
-    
-    const output:cdk.CfnOutputProps = {value:this.attr.attrArn,
-      exportName:"schedulerArn",
-      description:"shecduler One ARN"
-    }
-    new cdk.CfnOutput(this,"schedulerOutput",output)
-  }
 
-  
+    this.attr = new scheduler.CfnSchedule(
+      this,
+      "scheduleOne-1",
+      schedulerProperties
+    );
+
+    const output: cdk.CfnOutputProps = {
+      value: this.attr.attrArn,
+      exportName: "schedulerArn",
+      description: "shecduler One ARN",
+    };
+    new cdk.CfnOutput(this, "schedulerOutput", output);
+  }
 }
--- EventBridgeScheduler/test/event_bridge_scheduler.test.ts
+++ EventBridgeScheduler/test/event_bridge_scheduler.test.ts
@@ -1,26 +1,27 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
-import { EventBridgeScheduler } from '../lib/index';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { EventBridgeScheduler } from "../lib/index";
 
-test('EventBridge Schedule Created', () => {
-    const app = new cdk.App();
-    const stack = new cdk.Stack(app, "TestStack");
+test("EventBridge Schedule Created", () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, "TestStack");
 
-new EventBridgeScheduler(stack, 'MyTestConstruct',{schedulerProperties:{
-        name:"test-schedule",
-        flexibleTimeWindow:{mode:"OFF"},
-        scheduleExpression: "rate(1 hour)",
-        target:{input:'{data:"test"}',
-            arn:"rds:StartDbInstance",
-            roleArn:"role"
-            }
-}});
+  new EventBridgeScheduler(stack, "MyTestConstruct", {
+    schedulerProperties: {
+      name: "test-schedule",
+      flexibleTimeWindow: { mode: "OFF" },
+      scheduleExpression: "rate(1 hour)",
+      target: {
+        input: '{data:"test"}',
+        arn: "rds:StartDbInstance",
+        roleArn: "role",
+      },
+    },
+  });
 
-const template = Template.fromStack(stack);
+  const template = Template.fromStack(stack);
 
-
-
-  template.hasResourceProperties('AWS::Scheduler::Schedule',{
-        Name:"test-schedule"
-});
+  template.hasResourceProperties("AWS::Scheduler::Schedule", {
+    Name: "test-schedule",
+  });
 });
